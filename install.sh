7b77583a3b75623229b9b9f2b7c5a21bc55c201b
proxin187 <thugshaker627@gmail.com>
2025-12-26 21:48:58 +0100

    fix: reload shell to load new group

--- install.sh
+++ install.sh
@@ -1,68 +1,63 @@
 #!/bin/bash
 
-echo "Info: Starting Artx Setup"
 
-if ! groups | grep -q wheel; then
-    echo "Info: Adding user to wheel group"
-    sudo usermod -aG wheel $USER
-fi
+main_install() {
+    echo "Info: Installing doas"
+    sudo pacman -S --noconfirm opendoas
 
-echo "Info: Installing doas"
-sudo pacman -S --noconfirm opendoas
-
-cat << 'EOF' | sudo tee /etc/doas.conf
+    cat << 'EOF' | sudo tee /etc/doas.conf
 permit persist setenv {PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin} :wheel
 
 permit nopass :wheel as root cmd poweroff
 permit nopass :wheel as root cmd reboot
 EOF
-sudo chown -c root:root /etc/doas.conf
-sudo chmod -c 0400 /etc/doas.conf
+    sudo chown -c root:root /etc/doas.conf
+    sudo chmod -c 0400 /etc/doas.conf
 
-cat << 'EOF' | sudo tee /usr/local/bin/sudo
+    cat << 'EOF' | sudo tee /usr/local/bin/sudo
 #!/bin/bash
 doas "$@"
 EOF
-sudo chmod +x /usr/local/bin/sudo
+    sudo chmod +x /usr/local/bin/sudo
 
-doas pacman -R --noconfirm sudo
+    doas pacman -R --noconfirm sudo
 
-echo "Info: Installing yay AUR helper"
-doas pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git /tmp/yay && (cd /tmp/yay && makepkg -si --noconfirm)
+    echo "Info: Installing yay AUR helper"
+    doas pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git /tmp/yay && (cd /tmp/yay && makepkg -si --noconfirm)
 
-echo "Info: Installing base dependencies"
-doas pacman -S --noconfirm wireless_tools bash neovim yazi dmenu ttf-iosevka-nerd noto-fonts-emoji feh xorg-server xorg-xinit xorg-xsetroot libx11 libxft libxinerama libxrender libxcb alsa-utils alsa-utils-runit
-yay -S --noconfirm ttf-material-design-icons-desktop-git
+    echo "Info: Installing base dependencies"
+    doas pacman -S --noconfirm wireless_tools bash neovim yazi dmenu ttf-iosevka-nerd noto-fonts-emoji feh xorg-server xorg-xinit xorg-xsetroot libx11 libxft libxinerama libxrender libxcb alsa-utils alsa-utils-runit
+    yay -S --noconfirm ttf-material-design-icons-desktop-git
 
-echo "Info: Enabling alsa runit service"
-doas ln -s /etc/runit/sv/alsa /run/runit/service/
+    echo "Info: Enabling alsa runit service"
+    doas ln -s /etc/runit/sv/alsa /run/runit/service/
 
-echo "Info: Cloning into Artx and building packages"
-git clone https://github.com/proxin187/artx $HOME/.config/artx
-cd $HOME/.config/artx
+    echo "Info: Cloning into Artx and building packages"
+    git clone https://github.com/proxin187/artx $HOME/.config/artx
+    cd $HOME/.config/artx
 
-(cd dwm && doas make clean install)
-(cd st && doas make clean install)
+    (cd dwm && doas make clean install)
+    (cd st && doas make clean install)
 
-echo "Info: Setting up dotfiles"
-cat > $HOME/.xinitrc << 'EOF'
+    echo "Info: Setting up dotfiles"
+    cat > $HOME/.xinitrc << 'EOF'
 #!/bin/bash
 feh --bg-scale $HOME/.config/artx/wallpapers/wallpaper-5.jpg &
 exec dwm
 EOF
-chmod +x $HOME/.xinitrc
+    chmod +x $HOME/.xinitrc
 
-cat > $HOME/.bashrc << 'EOF'
+    cat > $HOME/.bashrc << 'EOF'
 [[ $- != *i* ]] && return
 PS1='\[\e[31m\][\[\e[33m\]\u\[\e[32m\]@\[\e[34m\]\h\[\e[0m\] \[\e[35m\]\W\[\e[31m\]]\[\e[0m\]\$ '
 alias ls='ls --color=auto'
 bind '"\e[A":history-search-backward'
 bind '"\e[B":history-search-forward'
 EOF
-chmod +x $HOME/.bashrc
+    chmod +x $HOME/.bashrc
 
-mkdir -p $HOME/.config/yazi
-cat > $HOME/.config/yazi/keymap.toml << 'EOF'
+    mkdir -p $HOME/.config/yazi
+    cat > $HOME/.config/yazi/keymap.toml << 'EOF'
 [mgr]
 prepend_keymap = [
     { on = "q", run = "quit", desc = "Quit" },
@@ -76,8 +71,8 @@ prepend_keymap = [
 ]
 EOF
 
-mkdir -p $HOME/.config/nvim
-cat > $HOME/.config/nvim/init.vim << 'EOF'
+    mkdir -p $HOME/.config/nvim
+    cat > $HOME/.config/nvim/init.vim << 'EOF'
 set nocompatible
 set showmatch
 set ignorecase
@@ -129,14 +124,25 @@ noremap t e
 noremap s b
 EOF
 
-cat > $HOME/.config/nvim/coc-settings.json << 'EOF'
+    cat > $HOME/.config/nvim/coc-settings.json << 'EOF'
 {
   "inlayHint.enable": false,
   "typescript.autoClosingTags": false
 }
 EOF
 
-echo "Info: The setup is done"
-echo "Note: Please restart your system before running startx"
+    echo "Info: The setup is done"
+    echo "Note: Please restart your system before running startx"
+}
+
+echo "Info: Starting Artx Setup"
+
+if ! groups | grep -q wheel; then
+    echo "Info: Adding user to wheel group"
+    sudo usermod -aG wheel $USER
+    exec su - $(whoami) -c "$(declare -f main_install); main_install"
+else
+    main_install
+fi
 
 
